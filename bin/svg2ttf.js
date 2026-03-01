47efb76d5e2dfb31a4d37bd7c953d2fb86db0072
Charlie-XIAO <yx2436@nyu.edu>
2026-03-01 17:04:32 -0500

    handle non-existent input directory more elegantly
    Signed-off-by: Charlie-XIAO <yx2436@nyu.edu>

--- bin/svg2ttf.js
+++ bin/svg2ttf.js
@@ -12,7 +12,7 @@ tmp.setGracefulCleanup();
 const program = new Command();
 
 program
-  .name("deskulpt-icons")
+  .name("svg2ttf")
   .description("Fix SVGs and generate icon fonts")
   .requiredOption("-i, --input <dir>", "input SVGs directory")
   .requiredOption("-o, --output <dir>", "output fonts directory")
@@ -36,6 +36,13 @@ const inputDir = path.resolve(process.cwd(), opts.input);
 const outputDir = path.resolve(process.cwd(), opts.output);
 
 async function main() {
+  try {
+    await fs.access(inputDir);
+  } catch (err) {
+    console.error(`Input directory does not exist: ${inputDir}`);
+    process.exit(1);
+  }
+
   const outlinedDir = tmp.dirSync({ unsafeCleanup: true });
 
   await SVGFixer(inputDir, outlinedDir.name, {
