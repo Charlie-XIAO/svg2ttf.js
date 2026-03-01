04e81c2754b054a540c097b00f8e1f9db6cb7fbd
Charlie-XIAO <yx2436@nyu.edu>
2026-03-01 11:24:39 -0500

    initial commit
    Signed-off-by: Charlie-XIAO <yx2436@nyu.edu>

---
+++ bin/svg2ttf.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+import path from "node:path";
+import fs from "node:fs/promises";
+import { Command } from "commander";
+import tmp from "tmp";
+import svgtofont from "svgtofont";
+import SVGFixer from "oslllo-svg-fixer";
+
+tmp.setGracefulCleanup();
+
+const program = new Command();
+
+program
+  .name("deskulpt-icons")
+  .description("Fix SVGs and generate icon fonts")
+  .requiredOption("-i, --input <dir>", "input SVGs directory")
+  .requiredOption("-o, --output <dir>", "output fonts directory")
+  .requiredOption("-n, --font-name <name>", "font name")
+  .option(
+    "--trace-resolution [n]",
+    "svg fixer trace resolution",
+    (v) => {
+      const n = Number(v);
+      if (!Number.isFinite(n) || n <= 0)
+        throw new Error("trace-resolution must be a positive number");
+      return n;
+    },
+    800,
+  );
+
+program.parse(process.argv);
+const opts = program.opts();
+
+const inputDir = path.resolve(process.cwd(), opts.input);
+const outputDir = path.resolve(process.cwd(), opts.output);
+
+async function main() {
+  const outlinedDir = tmp.dirSync({ unsafeCleanup: true });
+
+  await SVGFixer(inputDir, outlinedDir.name, {
+    showProgressBar: true,
+    traceResolution: opts.traceResolution,
+  }).fix();
+
+  await svgtofont({
+    src: outlinedDir.name,
+    dist: outputDir,
+    emptyDist: true,
+    fontName: opts.fontName,
+    css: false,
+    svgicons2svgfont: {
+      fontHeight: 1000,
+      normalize: false,
+    },
+    generateInfoData: true,
+    excludeFormat: ["eot", "svg", "symbol.svg", "woff", "woff2"],
+  });
+
+  outlinedDir.removeCallback();
+
+  process.stdout.write(`Done: ${outputDir}\n`);
+}
+
+await main();
